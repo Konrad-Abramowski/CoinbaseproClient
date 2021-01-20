7bf391cb3f7b75268bbb666abb11460c3af983c8
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-01-20 21:24:02 +0100

    Initial commit

---
+++ src/main/java/com/example/coinbaseprowebsocket/CoinbaseprowebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinbaseprowebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbaseprowebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinbaseprowebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coinbaseprowebsocket/CoinbaseprowebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinbaseprowebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinbaseprowebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
